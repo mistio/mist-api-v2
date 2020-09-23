dcfb78eb83a34699322675710f12c7f778292ba8
Dimitris Moraitis <dimo@mist.io>
2020-09-23 23:08:24 +0000

    Regenerate api-v2 after: Add features to cloud response object

--- mist_api_v2/models/__init__.py
+++ mist_api_v2/models/__init__.py
@@ -4,12 +4,15 @@
 from __future__ import absolute_import
 # import models into model package
 from mist_api_v2.models.add_cloud_request import AddCloudRequest
-from mist_api_v2.models.add_cloud_request_features import AddCloudRequestFeatures
 from mist_api_v2.models.amazon_credentials import AmazonCredentials
 from mist_api_v2.models.cloud import Cloud
 from mist_api_v2.models.cloud_credentials import CloudCredentials
+from mist_api_v2.models.cloud_features import CloudFeatures
+from mist_api_v2.models.digital_ocean_credentials import DigitalOceanCredentials
 from mist_api_v2.models.google_credentials import GoogleCredentials
 from mist_api_v2.models.inline_response200 import InlineResponse200
 from mist_api_v2.models.list_clouds_response import ListCloudsResponse
 from mist_api_v2.models.open_stack_credentials import OpenStackCredentials
+from mist_api_v2.models.packet_credentials import PacketCredentials
 from mist_api_v2.models.response_metadata import ResponseMetadata
+from mist_api_v2.models.v_sphere_credentials import VSphereCredentials
