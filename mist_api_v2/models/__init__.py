e8136b8a3b0c4b3ca3bd6c8b23d3d4d7083bd68e
Mist Gitlab Runner <gitlab.ops.mist.io@gmail.com>
2020-09-27 13:49:13 +0000

    Regenerate api-v2 after: Add new models before commiting

--- mist_api_v2/models/__init__.py
+++ mist_api_v2/models/__init__.py
@@ -5,19 +5,22 @@ from __future__ import absolute_import
 # import models into model package
 from mist_api_v2.models.action import Action
 from mist_api_v2.models.add_cloud_request import AddCloudRequest
-from mist_api_v2.models.add_cloud_request_features import AddCloudRequestFeatures
 from mist_api_v2.models.amazon_credentials import AmazonCredentials
 from mist_api_v2.models.cloud import Cloud
 from mist_api_v2.models.cloud_credentials import CloudCredentials
+from mist_api_v2.models.cloud_features import CloudFeatures
+from mist_api_v2.models.digital_ocean_credentials import DigitalOceanCredentials
 from mist_api_v2.models.frequency import Frequency
-from mist_api_v2.models.get_rules_response import GetRulesResponse
 from mist_api_v2.models.google_credentials import GoogleCredentials
 from mist_api_v2.models.inline_response200 import InlineResponse200
 from mist_api_v2.models.list_clouds_response import ListCloudsResponse
+from mist_api_v2.models.list_rules_response import ListRulesResponse
 from mist_api_v2.models.open_stack_credentials import OpenStackCredentials
+from mist_api_v2.models.packet_credentials import PacketCredentials
 from mist_api_v2.models.query import Query
 from mist_api_v2.models.response_metadata import ResponseMetadata
 from mist_api_v2.models.rule import Rule
 from mist_api_v2.models.selector import Selector
 from mist_api_v2.models.trigger_after import TriggerAfter
+from mist_api_v2.models.v_sphere_credentials import VSphereCredentials
 from mist_api_v2.models.window import Window
