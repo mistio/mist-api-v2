885b3a1c03e33ab933fdc7d8b1549a409d131a9a
Dimitris Moraitis <dimo@mist.io>
2020-12-07 14:56:55 +0200

    Fix returned items, set deref to auto

--- mist_api_v2/controllers/base.py
+++ mist_api_v2/controllers/base.py
@@ -4,14 +4,13 @@ def list_resources(auth_context, resource_type, cloud=None, search='', only='',
         List resources of any type and prepare the HTTP response
     """
     from mist.api.methods import list_resources
-
     items, total = list_resources(
         auth_context, resource_type, cloud=cloud, search=search, only=only,
         sort=sort, limit=limit
     )
     meta = {
         'total': total,
-        'returned': items.count(),
+        'returned': len(items),
         'sort': sort,
         'start': start
     }
