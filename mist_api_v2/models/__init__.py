fe0441c5562d8e31ca1280eb32f3af6a739b07b2
Mist Gitlab Runner <gitlab.ops.mist.io@gmail.com>
2021-06-10 14:03:27 +0000

    Regenerate api-v2 after: Add delete-volume

--- mist_api_v2/models/__init__.py
+++ mist_api_v2/models/__init__.py
@@ -60,6 +60,7 @@ from mist_api_v2.models.expiration_notify import ExpirationNotify
 from mist_api_v2.models.frequency import Frequency
 from mist_api_v2.models.get_cloud_response import GetCloudResponse
 from mist_api_v2.models.get_image_response import GetImageResponse
+from mist_api_v2.models.get_job_response import GetJobResponse
 from mist_api_v2.models.get_key_response import GetKeyResponse
 from mist_api_v2.models.get_location_response import GetLocationResponse
 from mist_api_v2.models.get_machine_response import GetMachineResponse
@@ -77,6 +78,7 @@ from mist_api_v2.models.inline_object import InlineObject
 from mist_api_v2.models.inline_response200 import InlineResponse200
 from mist_api_v2.models.inline_script import InlineScript
 from mist_api_v2.models.interval_schedule import IntervalSchedule
+from mist_api_v2.models.job import Job
 from mist_api_v2.models.kvm_net import KVMNet
 from mist_api_v2.models.kvm_net_networks import KVMNetNetworks
 from mist_api_v2.models.key import Key
@@ -96,6 +98,7 @@ from mist_api_v2.models.list_snapshots_response import ListSnapshotsResponse
 from mist_api_v2.models.list_volumes_response import ListVolumesResponse
 from mist_api_v2.models.list_zones_response import ListZonesResponse
 from mist_api_v2.models.location import Location
+from mist_api_v2.models.log import Log
 from mist_api_v2.models.lxd_credentials import LxdCredentials
 from mist_api_v2.models.machine import Machine
 from mist_api_v2.models.machine_state import MachineState
