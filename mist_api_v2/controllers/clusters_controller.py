af1fc994eea38ce60a292874d22ddebd9e52eda6
Christos Karageorgiou Kaneen <csk@mist.io>
2021-07-12 08:54:51 +0000

    Fix flake8 errors

--- mist_api_v2/controllers/clusters_controller.py
+++ mist_api_v2/controllers/clusters_controller.py
@@ -1,25 +1,13 @@
-import connexion
-import six
-
-from mist_api_v2.models.create_cluster_request import CreateClusterRequest  # noqa: E501
-from mist_api_v2.models.get_cluster_response import GetClusterResponse  # noqa: E501
-from mist_api_v2.models.inline_response200 import InlineResponse200  # noqa: E501
-from mist_api_v2.models.list_clusters_response import ListClustersResponse  # noqa: E501
-from mist_api_v2 import util
-
-
 def create_cluster(create_cluster_request=None):  # noqa: E501
     """Create cluster
 
     Create a new cluster and return the cluster&#39;s id # noqa: E501
 
-    :param create_cluster_request: 
+    :param create_cluster_request:
     :type create_cluster_request: dict | bytes
 
     :rtype: InlineResponse200
     """
-    if connexion.request.is_json:
-        create_cluster_request = CreateClusterRequest.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
@@ -28,7 +16,7 @@ def delete_cluster(cluster):  # noqa: E501
 
     Delete target clusters # noqa: E501
 
-    :param cluster: 
+    :param cluster:
     :type cluster: str
 
     :rtype: None
@@ -41,7 +29,7 @@ def get_cluster(cluster, only=None, deref=None):  # noqa: E501
 
     Get details about target cluster # noqa: E501
 
-    :param cluster: 
+    :param cluster:
     :type cluster: str
     :param only: Only return these fields
     :type only: str
@@ -58,7 +46,7 @@ def list_clusters(cloud=None, search=None, sort=None, start=None, limit=None, on
 
     List clusters # noqa: E501
 
-    :param cloud: 
+    :param cloud:
     :type cloud: str
     :param search: Only return results matching search filter
     :type search: str
