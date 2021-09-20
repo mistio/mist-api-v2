76cf2cd1ad1f906cde36b0c060d9c1453b432e85
Mist Gitlab Runner <gitlab.ops.mist.io@gmail.com>
2021-09-20 10:05:04 +0000

    Regenerate api-v2 after: Add vexxhost provider

--- mist_api_v2/models/add_cloud_request.py
+++ mist_api_v2/models/add_cloud_request.py
@@ -104,7 +104,7 @@ class AddCloudRequest(Model):
         :param provider: The provider of this AddCloudRequest.
         :type provider: str
         """
-        allowed_values = ["alibaba", "amazon", "azure", "cloudsigma", "digitalocean", "equinix", "google", "ibm", "kvm", "kubevirt", "kubernetes", "linode", "lxd", "maxihost", "onapp", "openstack", "openshift", "other", "rackspace", "vcloud", "vsphere", "vultr"]  # noqa: E501
+        allowed_values = ["alibaba", "amazon", "azure", "cloudsigma", "digitalocean", "equinix", "google", "ibm", "kvm", "kubevirt", "kubernetes", "linode", "lxd", "maxihost", "onapp", "openstack", "openshift", "other", "rackspace", "vcloud", "vexxhost", "vsphere", "vultr"]  # noqa: E501
         if provider not in allowed_values:
             raise ValueError(
                 "Invalid value for `provider` ({0}), must be one of {1}"
