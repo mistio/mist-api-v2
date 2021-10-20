ee0a6f8f3f6f092a85925b2ed14be045b40cd96b
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-10-20 20:54:49 +0300

    Implement revert_to_snapshot()

--- mist_api_v2/controllers/snapshots_controller.py
+++ mist_api_v2/controllers/snapshots_controller.py
@@ -88,4 +88,15 @@ def revert_to_snapshot(machine, snapshot):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    from mist.api.methods import list_resources
+    auth_context = connexion.context['token_info']['auth_context']
+    try:
+        [machine], total = list_resources(
+            auth_context, 'machine', search=machine, limit=1)
+    except ValueError:
+        return 'Machine does not exist', 404
+    auth_context.check_perm('machine', 'read', machine.id)
+    auth_context.check_perm('machine', 'list_snapshots', machine.id)
+    auth_context.check_perm('machine', 'revert_to_snapshots', machine.id)
+    machine.ctl.revert_to_snapshot(snapshot)
+    return 'Revert machine to snapshot issued successfully'
