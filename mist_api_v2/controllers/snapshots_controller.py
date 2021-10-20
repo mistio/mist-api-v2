031a70d646805e10449663356eeab27fdc636aa0
Chris Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-10-20 20:44:38 +0300

    Implement create_snapshot()

--- mist_api_v2/controllers/snapshots_controller.py
+++ mist_api_v2/controllers/snapshots_controller.py
@@ -1,17 +1,30 @@
 import connexion
 
 
-def create_snapshot(machine):  # noqa: E501
+def create_snapshot(machine, name):  # noqa: E501
     """Create snapshot
 
     Create snapshots of target machine # noqa: E501
 
     :param machine:
     :type machine: str
+    :param name:
+    :type name: str
 
     :rtype: object
     """
-    return 'do some magic!'
+    from mist.api.methods import list_resources
+    auth_context = connexion.context['token_info']['auth_context']
+    try:
+        [machine], total = list_resources(
+            auth_context, 'machine', search=machine, limit=1)
+    except ValueError:
+        return 'Machine does not exist', 404
+    auth_context.check_perm('machine', 'read', machine.id)
+    auth_context.check_perm('machine', 'list_snapshots', machine.id)
+    auth_context.check_perm('machine', 'create_snapshots', machine.id)
+    machine.ctl.create_snapshot(name)
+    return {'name': name}
 
 
 def list_snapshots(machine):  # noqa: E501
