1ef9ff4136393f79a794273817b63a05292ed2b6
Christos Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2021-12-15 23:07:28 +0200

    Improve networks ctl error handling

--- mist_api_v2/controllers/networks_controller.py
+++ mist_api_v2/controllers/networks_controller.py
@@ -4,6 +4,11 @@ from mist.api.networks.models import NETWORKS
 from mist.api.helpers import delete_none
 from mist.api.tag.methods import add_tags_to_resource
 
+from mist.api.exceptions import BadRequestError
+from mist.api.exceptions import PolicyUnauthorizedError
+from mist.api.exceptions import MistError
+from mist.api.exceptions import NetworkListingError
+
 from mist_api_v2.models.create_network_request import CreateNetworkRequest  # noqa: E501
 from mist_api_v2.models.get_network_response import GetNetworkResponse  # noqa: E501
 from mist_api_v2.models.list_networks_response import ListNetworksResponse  # noqa: E501
@@ -24,23 +29,38 @@ def create_network(create_network_request=None):  # noqa: E501
     from mist.api.methods import list_resources
     if connexion.request.is_json:
         create_network_request = CreateNetworkRequest.from_dict(connexion.request.get_json())  # noqa: E501
-    auth_context = connexion.context['token_info']['auth_context']
+    try:
+        auth_context = connexion.context['token_info']['auth_context']
+    except KeyError:
+        return 'Authentication failed', 401
     params = delete_none(create_network_request.to_dict())
-    tags, _ = auth_context.check_perm("network", "add", None)
+    try:
+        tags, _ = auth_context.check_perm("network", "add", None)
+    except PolicyUnauthorizedError:
+        return 'You are not authorized to perform this action', 403
     try:
         [cloud], total = list_resources(
             auth_context, 'cloud', search=params.pop('cloud'), limit=1)
     except ValueError:
         return 'Cloud does not exist', 404
-    auth_context.check_perm('cloud', 'read', cloud.id)
-    auth_context.check_perm('cloud', 'create_resources', cloud.id)
+    try:
+        auth_context.check_perm('cloud', 'read', cloud.id)
+        auth_context.check_perm('cloud', 'create_resources', cloud.id)
+    except PolicyUnauthorizedError:
+        return 'You are not authorized to perform this action', 403
     if not hasattr(cloud.ctl, 'network'):
         return 'Network support is not available', 501
     # Create the new network
     network_params = {'name': params['name']}
     if 'extra' in params:
         network_params.update(params['extra'])
-    network = NETWORKS[cloud.ctl.provider].add(cloud=cloud, **network_params)
+    try:
+        network = NETWORKS[cloud.ctl.provider].add(
+            cloud=cloud, **network_params)
+    except BadRequestError as e:
+        return str(e), 400
+    except NetworkListingError as e:
+        return str(e), 503
     network.assign_to(auth_context.user)
     if tags:
         add_tags_to_resource(auth_context.owner, network, tags)
@@ -60,7 +80,10 @@ def delete_network(network, cloud):  # noqa: E501
     :rtype: None
     """
     from mist.api.methods import list_resources
-    auth_context = connexion.context['token_info']['auth_context']
+    try:
+        auth_context = connexion.context['token_info']['auth_context']
+    except KeyError:
+        return 'Authentication failed', 401
     try:
         [cloud], total = list_resources(
             auth_context, 'cloud', search=cloud, limit=1)
@@ -71,10 +94,16 @@ def delete_network(network, cloud):  # noqa: E501
             auth_context, 'network', search=network, limit=1)
     except ValueError:
         return 'Network does not exist', 404
-    auth_context.check_perm("cloud", "read", cloud.id)
-    auth_context.check_perm('network', 'read', network.id)
-    auth_context.check_perm('network', 'remove', network.id)
-    network.ctl.delete()
+    try:
+        auth_context.check_perm("cloud", "read", cloud.id)
+        auth_context.check_perm('network', 'read', network.id)
+        auth_context.check_perm('network', 'remove', network.id)
+    except PolicyUnauthorizedError:
+        return 'You are not authorized to perform this action', 403
+    try:
+        network.ctl.delete()
+    except MistError as e:
+        return str(e), 500
     return 'Network deleted succesfully', 200
 
 
@@ -91,13 +120,19 @@ def edit_network(network, name=None):  # noqa: E501
     :rtype: None
     """
     from mist.api.methods import list_resources
-    auth_context = connexion.context['token_info']['auth_context']
+    try:
+        auth_context = connexion.context['token_info']['auth_context']
+    except KeyError:
+        return 'Authentication failed', 401
     try:
         [network], total = list_resources(
             auth_context, 'network', search=network, limit=1)
     except ValueError:
         return 'Network does not exist', 404
-    auth_context.check_perm('network', 'edit', network.id)
+    try:
+        auth_context.check_perm('network', 'edit', network.id)
+    except PolicyUnauthorizedError:
+        return 'You are not authorized to perform this action', 403
     network.ctl.rename(name)
     return 'Network succesfully updated'
 
@@ -116,7 +151,10 @@ def get_network(network, only=None, deref='auto'):  # noqa: E501
 
     :rtype: GetNetworkResponse
     """
-    auth_context = connexion.context['token_info']['auth_context']
+    try:
+        auth_context = connexion.context['token_info']['auth_context']
+    except KeyError:
+        return 'Authentication failed', 401
     result = get_resource(
         auth_context, 'network', search=network, only=only, deref=deref)
     return GetNetworkResponse(data=result['data'], meta=result['meta'])
@@ -144,7 +182,10 @@ def list_networks(cloud=None, search=None, sort=None, start=None, limit=None, on
 
     :rtype: ListNetworksResponse
     """
-    auth_context = connexion.context['token_info']['auth_context']
+    try:
+        auth_context = connexion.context['token_info']['auth_context']
+    except KeyError:
+        return 'Authentication failed', 401
     result = list_resources(
         auth_context, 'network', cloud=cloud, search=search, only=only,
         sort=sort, start=start, limit=limit, deref=deref)
