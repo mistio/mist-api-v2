16b8236b91a18e3007ef281ca72c671a60c53d09
AlAtsal <alexandros_atsalakis@outlook.com>
2022-03-16 09:48:58 +0000

    migrated get_tags functionality to mist.api.tags.methods

--- mist_api_v2/controllers/tags_controller.py
+++ mist_api_v2/controllers/tags_controller.py
@@ -2,11 +2,10 @@ import connexion
 
 
 from mist_api_v2.models.list_tags_response import ListTagsResponse  # noqa: E501
-from mist.api.tag.models import Tag
-from mongoengine import Q
+from mist.api.tag.methods import get_tags
 
 
-def list_tags(verbose=None, resource=None, search=None, sort=None, start=None, limit=None, only=None, deref=None):  # noqa: E501
+def list_tags(verbose=None, resource=None, search='', sort='key', start=0, limit=100, only='', deref=None):  # noqa: E501
     """List tags
 
     List tags on resources owned by the active org. READ permission required on each resource. # noqa: E501
@@ -30,31 +29,15 @@ def list_tags(verbose=None, resource=None, search=None, sort=None, start=None, l
 
     :rtype: ListTagsResponse
     """
-    # import ipdb; ipdb.set_trace()
+
     try:
         auth_context = connexion.context['token_info']['auth_context']
     except KeyError:
         return 'Authentication failed', 401
 
-    query = Q(owner=auth_context.owner)
-    if resource:
-        query &= Q(resource_type=resource.rstrip('s'))
-
-    tags = Tag.objects(query)
-    data = [{'key': k, 'value': v} for k, v in
-            set((t.key, t.value) for t in tags)]
-    if verbose:
-        for kv in data:
-            kv_temp = kv.copy()
-            for resource_type in tags.filter(**kv_temp).distinct('resource_type'):  # noqa: E501
-                kv[resource_type + 's'] = [tag.resource_id for tag in
-                                           Tag.objects(**kv_temp, resource_type=resource_type)]  # noqa: E501
-
-    meta = {
-        'total': len(data),
-        'returned': len(data),
-        'sort': sort,
-        'start': start
-    }
+    #  import ipdb; ipdb.set_trace()
+    data, meta = get_tags(auth_context, verbose=verbose, resource=resource,
+                          search=search, sort=sort, start=start, limit=limit,
+                          only=only, deref=deref)
 
     return ListTagsResponse(data, meta)
