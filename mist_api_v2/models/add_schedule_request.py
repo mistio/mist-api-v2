d7f90b73aea1d6e4a9d5b51ac885108b8420420c
Mist Gitlab Runner <gitlab.ops.mist.io@gmail.com>
2022-03-17 09:20:51 +0000

    Regenerate api-v2 after: Updated specification for add schedule endpoint

--- mist_api_v2/models/add_schedule_request.py
+++ mist_api_v2/models/add_schedule_request.py
@@ -15,46 +15,71 @@ class AddScheduleRequest(Model):
     Do not edit the class manually.
     """
 
-    def __init__(self, name=None, schedule=None, location_type=None, entrypoint=None, exec_type=None, description=None):  # noqa: E501
+    def __init__(self, name=None, description=None, task_enabled=None, action=None, params=None, resource_type=None, selectors=None, schedule_type=None, schedule_entry=None, start_after=None, run_immediately=None):  # noqa: E501
         """AddScheduleRequest - a model defined in OpenAPI
 
         :param name: The name of this AddScheduleRequest.  # noqa: E501
         :type name: str
-        :param schedule: The schedule of this AddScheduleRequest.  # noqa: E501
-        :type schedule: str
-        :param location_type: The location_type of this AddScheduleRequest.  # noqa: E501
-        :type location_type: str
-        :param entrypoint: The entrypoint of this AddScheduleRequest.  # noqa: E501
-        :type entrypoint: str
-        :param exec_type: The exec_type of this AddScheduleRequest.  # noqa: E501
-        :type exec_type: str
         :param description: The description of this AddScheduleRequest.  # noqa: E501
         :type description: str
+        :param task_enabled: The task_enabled of this AddScheduleRequest.  # noqa: E501
+        :type task_enabled: bool
+        :param action: The action of this AddScheduleRequest.  # noqa: E501
+        :type action: str
+        :param params: The params of this AddScheduleRequest.  # noqa: E501
+        :type params: str
+        :param resource_type: The resource_type of this AddScheduleRequest.  # noqa: E501
+        :type resource_type: str
+        :param selectors: The selectors of this AddScheduleRequest.  # noqa: E501
+        :type selectors: List[str]
+        :param schedule_type: The schedule_type of this AddScheduleRequest.  # noqa: E501
+        :type schedule_type: str
+        :param schedule_entry: The schedule_entry of this AddScheduleRequest.  # noqa: E501
+        :type schedule_entry: str
+        :param start_after: The start_after of this AddScheduleRequest.  # noqa: E501
+        :type start_after: str
+        :param run_immediately: The run_immediately of this AddScheduleRequest.  # noqa: E501
+        :type run_immediately: bool
         """
         self.openapi_types = {
             'name': str,
-            'schedule': str,
-            'location_type': str,
-            'entrypoint': str,
-            'exec_type': str,
-            'description': str
+            'description': str,
+            'task_enabled': bool,
+            'action': str,
+            'params': str,
+            'resource_type': str,
+            'selectors': List[str],
+            'schedule_type': str,
+            'schedule_entry': str,
+            'start_after': str,
+            'run_immediately': bool
         }
 
         self.attribute_map = {
             'name': 'name',
-            'schedule': 'schedule',
-            'location_type': 'location_type',
-            'entrypoint': 'entrypoint',
-            'exec_type': 'exec_type',
-            'description': 'description'
+            'description': 'description',
+            'task_enabled': 'task_enabled',
+            'action': 'action',
+            'params': 'params',
+            'resource_type': 'resource_type',
+            'selectors': 'selectors',
+            'schedule_type': 'schedule_type',
+            'schedule_entry': 'schedule_entry',
+            'start_after': 'start_after',
+            'run_immediately': 'run_immediately'
         }
 
         self._name = name
-        self._schedule = schedule
-        self._location_type = location_type
-        self._entrypoint = entrypoint
-        self._exec_type = exec_type
         self._description = description
+        self._task_enabled = task_enabled
+        self._action = action
+        self._params = params
+        self._resource_type = resource_type
+        self._selectors = selectors
+        self._schedule_type = schedule_type
+        self._schedule_entry = schedule_entry
+        self._start_after = start_after
+        self._run_immediately = run_immediately
 
     @classmethod
     def from_dict(cls, dikt) -> 'AddScheduleRequest':
@@ -91,104 +116,6 @@ class AddScheduleRequest(Model):
         self._name = name
 
     @property
-    def schedule(self):
-        """Gets the schedule of this AddScheduleRequest.
-
-
-        :return: The schedule of this AddScheduleRequest.
-        :rtype: str
-        """
-        return self._schedule
-
-    @schedule.setter
-    def schedule(self, schedule):
-        """Sets the schedule of this AddScheduleRequest.
-
-
-        :param schedule: The schedule of this AddScheduleRequest.
-        :type schedule: str
-        """
-        if schedule is None:
-            raise ValueError("Invalid value for `schedule`, must not be `None`")  # noqa: E501
-
-        self._schedule = schedule
-
-    @property
-    def location_type(self):
-        """Gets the location_type of this AddScheduleRequest.
-
-
-        :return: The location_type of this AddScheduleRequest.
-        :rtype: str
-        """
-        return self._location_type
-
-    @location_type.setter
-    def location_type(self, location_type):
-        """Sets the location_type of this AddScheduleRequest.
-
-
-        :param location_type: The location_type of this AddScheduleRequest.
-        :type location_type: str
-        """
-        allowed_values = ["inline", "github", "url"]  # noqa: E501
-        if location_type not in allowed_values:
-            raise ValueError(
-                "Invalid value for `location_type` ({0}), must be one of {1}"
-                .format(location_type, allowed_values)
-            )
-
-        self._location_type = location_type
-
-    @property
-    def entrypoint(self):
-        """Gets the entrypoint of this AddScheduleRequest.
-
-
-        :return: The entrypoint of this AddScheduleRequest.
-        :rtype: str
-        """
-        return self._entrypoint
-
-    @entrypoint.setter
-    def entrypoint(self, entrypoint):
-        """Sets the entrypoint of this AddScheduleRequest.
-
-
-        :param entrypoint: The entrypoint of this AddScheduleRequest.
-        :type entrypoint: str
-        """
-
-        self._entrypoint = entrypoint
-
-    @property
-    def exec_type(self):
-        """Gets the exec_type of this AddScheduleRequest.
-
-
-        :return: The exec_type of this AddScheduleRequest.
-        :rtype: str
-        """
-        return self._exec_type
-
-    @exec_type.setter
-    def exec_type(self, exec_type):
-        """Sets the exec_type of this AddScheduleRequest.
-
-
-        :param exec_type: The exec_type of this AddScheduleRequest.
-        :type exec_type: str
-        """
-        allowed_values = ["executable", "ansible"]  # noqa: E501
-        if exec_type not in allowed_values:
-            raise ValueError(
-                "Invalid value for `exec_type` ({0}), must be one of {1}"
-                .format(exec_type, allowed_values)
-            )
-
-        self._exec_type = exec_type
-
-    @property
     def description(self):
         """Gets the description of this AddScheduleRequest.
 
@@ -208,3 +135,218 @@ class AddScheduleRequest(Model):
         """
 
         self._description = description
+
+    @property
+    def task_enabled(self):
+        """Gets the task_enabled of this AddScheduleRequest.
+
+
+        :return: The task_enabled of this AddScheduleRequest.
+        :rtype: bool
+        """
+        return self._task_enabled
+
+    @task_enabled.setter
+    def task_enabled(self, task_enabled):
+        """Sets the task_enabled of this AddScheduleRequest.
+
+
+        :param task_enabled: The task_enabled of this AddScheduleRequest.
+        :type task_enabled: bool
+        """
+
+        self._task_enabled = task_enabled
+
+    @property
+    def action(self):
+        """Gets the action of this AddScheduleRequest.
+
+
+        :return: The action of this AddScheduleRequest.
+        :rtype: str
+        """
+        return self._action
+
+    @action.setter
+    def action(self, action):
+        """Sets the action of this AddScheduleRequest.
+
+
+        :param action: The action of this AddScheduleRequest.
+        :type action: str
+        """
+        allowed_values = ["start", "stop", "reboot", "destroy", "run script"]  # noqa: E501
+        if action not in allowed_values:
+            raise ValueError(
+                "Invalid value for `action` ({0}), must be one of {1}"
+                .format(action, allowed_values)
+            )
+
+        self._action = action
+
+    @property
+    def params(self):
+        """Gets the params of this AddScheduleRequest.
+
+
+        :return: The params of this AddScheduleRequest.
+        :rtype: str
+        """
+        return self._params
+
+    @params.setter
+    def params(self, params):
+        """Sets the params of this AddScheduleRequest.
+
+
+        :param params: The params of this AddScheduleRequest.
+        :type params: str
+        """
+
+        self._params = params
+
+    @property
+    def resource_type(self):
+        """Gets the resource_type of this AddScheduleRequest.
+
+
+        :return: The resource_type of this AddScheduleRequest.
+        :rtype: str
+        """
+        return self._resource_type
+
+    @resource_type.setter
+    def resource_type(self, resource_type):
+        """Sets the resource_type of this AddScheduleRequest.
+
+
+        :param resource_type: The resource_type of this AddScheduleRequest.
+        :type resource_type: str
+        """
+        allowed_values = ["machines", "volumes", "clusters", "networks"]  # noqa: E501
+        if resource_type not in allowed_values:
+            raise ValueError(
+                "Invalid value for `resource_type` ({0}), must be one of {1}"
+                .format(resource_type, allowed_values)
+            )
+
+        self._resource_type = resource_type
+
+    @property
+    def selectors(self):
+        """Gets the selectors of this AddScheduleRequest.
+
+        IDs of resources related to schedule  # noqa: E501
+
+        :return: The selectors of this AddScheduleRequest.
+        :rtype: List[str]
+        """
+        return self._selectors
+
+    @selectors.setter
+    def selectors(self, selectors):
+        """Sets the selectors of this AddScheduleRequest.
+
+        IDs of resources related to schedule  # noqa: E501
+
+        :param selectors: The selectors of this AddScheduleRequest.
+        :type selectors: List[str]
+        """
+        if selectors is None:
+            raise ValueError("Invalid value for `selectors`, must not be `None`")  # noqa: E501
+
+        self._selectors = selectors
+
+    @property
+    def schedule_type(self):
+        """Gets the schedule_type of this AddScheduleRequest.
+
+
+        :return: The schedule_type of this AddScheduleRequest.
+        :rtype: str
+        """
+        return self._schedule_type
+
+    @schedule_type.setter
+    def schedule_type(self, schedule_type):
+        """Sets the schedule_type of this AddScheduleRequest.
+
+
+        :param schedule_type: The schedule_type of this AddScheduleRequest.
+        :type schedule_type: str
+        """
+        allowed_values = ["crontab", "interval", "one_off"]  # noqa: E501
+        if schedule_type not in allowed_values:
+            raise ValueError(
+                "Invalid value for `schedule_type` ({0}), must be one of {1}"
+                .format(schedule_type, allowed_values)
+            )
+
+        self._schedule_type = schedule_type
+
+    @property
+    def schedule_entry(self):
+        """Gets the schedule_entry of this AddScheduleRequest.
+
+        format should be ΥΥΥΥ-ΜΜ-DD HH:MM:SS  # noqa: E501
+
+        :return: The schedule_entry of this AddScheduleRequest.
+        :rtype: str
+        """
+        return self._schedule_entry
+
+    @schedule_entry.setter
+    def schedule_entry(self, schedule_entry):
+        """Sets the schedule_entry of this AddScheduleRequest.
+
+        format should be ΥΥΥΥ-ΜΜ-DD HH:MM:SS  # noqa: E501
+
+        :param schedule_entry: The schedule_entry of this AddScheduleRequest.
+        :type schedule_entry: str
+        """
+
+        self._schedule_entry = schedule_entry
+
+    @property
+    def start_after(self):
+        """Gets the start_after of this AddScheduleRequest.
+
+        format should be ΥΥΥΥ-ΜΜ-DD HH:MM:SS  # noqa: E501
+
+        :return: The start_after of this AddScheduleRequest.
+        :rtype: str
+        """
+        return self._start_after
+
+    @start_after.setter
+    def start_after(self, start_after):
+        """Sets the start_after of this AddScheduleRequest.
+
+        format should be ΥΥΥΥ-ΜΜ-DD HH:MM:SS  # noqa: E501
+
+        :param start_after: The start_after of this AddScheduleRequest.
+        :type start_after: str
+        """
+
+        self._start_after = start_after
+
+    @property
+    def run_immediately(self):
+        """Gets the run_immediately of this AddScheduleRequest.
+
+
+        :return: The run_immediately of this AddScheduleRequest.
+        :rtype: bool
+        """
+        return self._run_immediately
+
+    @run_immediately.setter
+    def run_immediately(self, run_immediately):
+        """Sets the run_immediately of this AddScheduleRequest.
+
+
+        :param run_immediately: The run_immediately of this AddScheduleRequest.
+        :type run_immediately: bool
+        """
+
+        self._run_immediately = run_immediately
