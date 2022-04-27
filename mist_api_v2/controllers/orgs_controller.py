b8913349d9d124cd25179f0046d981dfbd92ac18
Eis <monoatomic.alex@gmail.com>
2022-04-27 11:47:19 +0300

    Add count for resources under the get org response

--- mist_api_v2/controllers/orgs_controller.py
+++ mist_api_v2/controllers/orgs_controller.py
@@ -8,7 +8,7 @@ from mist_api_v2.models.list_org_members_response import ListOrgMembersResponse 
 from mist_api_v2.models.list_org_teams_response import ListOrgTeamsResponse  # noqa: E501
 from mist_api_v2.models.list_orgs_response import ListOrgsResponse  # noqa: E501
 
-from .base import list_resources, get_resource
+from .base import list_resources, get_resource, get_org_resources_summary
 
 
 def get_member(org, member, only=None):  # noqa: E501
@@ -52,7 +52,7 @@ def get_member(org, member, only=None):  # noqa: E501
     return GetOrgMemberResponse(data=result['data'], meta=result['meta'])
 
 
-def get_org(org, only=None, deref=None):  # noqa: E501
+def get_org(org, summary=False, only=None, deref=None):  # noqa: E501
     """Get Org
 
     Get details about target org # noqa: E501
@@ -71,10 +71,11 @@ def get_org(org, only=None, deref=None):  # noqa: E501
     except KeyError:
         return 'Authentication failed', 401
     search = f'id={org}'
-    try:
-        result = get_resource(auth_context, 'orgs', search=search, only=only)
-    except NotFoundError:
-        return 'Organization does not exist', 404
+    result = get_resource(auth_context, 'orgs', search=search, only=only)
+    # get resources count only if get_resource rbac checks pass
+    if summary and result['meta']['returned'] == 1:
+        r_count = get_org_resources_summary(auth_context, org_id=org)
+        result['data']['resources_count'] = r_count
     return GetOrgResponse(data=result['data'], meta=result['meta'])
 
 
